9f5f95ea02181bd11e52ec3a13ed2666c2b788af
ARUN L <108601293+Arun100203@users.noreply.github.com>
2023-01-16 06:07:07 +0530

    initial commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Admin\\Desktop\\flutter_windows_3.3.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Admin\\Desktop\\whatsapp\\Flames_Calculator" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Admin\\Desktop\\flutter_windows_3.3.0-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Admin\\Desktop\\whatsapp\\Flames_Calculator"
+  "FLUTTER_ROOT=C:\\Users\\Admin\\Desktop\\flutter_windows_3.3.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Admin\\Desktop\\whatsapp\\Flames_Calculator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Admin\\Desktop\\whatsapp\\Flames_Calculator"
+  "FLUTTER_TARGET=C:\\Users\\Admin\\Desktop\\whatsapp\\Flames_Calculator\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Admin\\Desktop\\whatsapp\\Flames_Calculator\\.dart_tool\\package_config.json"
+)
